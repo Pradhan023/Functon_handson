37ce30e8dac7211676ca8736523c605f744b11c7
Anish Pradhan <anishpradha5523@gmail.com>
2023-05-03 18:32:39 +0530

    Function-Handson

---
+++ functon.js
@@ -0,0 +1,99 @@
+// Q1
+// function no()
+// {
+//     let yes 
+//     console.log(typeof(yes));
+// }
+// no()
+
+// Q2
+// function sum(s1,s2)
+// {
+//     console.log(`Sum of ${s1},${s2} is ${s1+s2}`);
+// }
+// sum(3,4)
+
+// Q3
+// const arfun = (a,b) => a%b
+// console.log(arfun(6,10));
+
+// Q4
+// var x = 21;
+// var girl = function () {
+//     console.log(x);
+//     var x = 20;
+// };
+// girl ();
+
+// Q5
+// var x = 21;
+// girl ();
+// console.log(x)
+// function girl() {
+//     console.log(x);
+//     var x = 20;
+// };
+
+// Q6
+// var x = 21;
+// a();
+// b();
+
+//   function a() {
+    
+//    x = 20;
+//   console.log(x);
+// };
+//  function b() {
+    
+//     x = 40;
+//    console.log(x);
+// };
+
+// Q7 factorial 
+
+// const fact = (n) => 
+// {
+//     let num =1
+//     for(let i = 1 ; i <= n ; i++)
+//     {
+//         num = num * i;
+//     }
+//     return num
+// }
+// console.log(fact(5));
+
+// Q8
+// function FindSum(a, b){
+//     return a + b;
+// }
+
+// function DisplayData(data, batch){
+//     console.log(`i am from ${data} and My batch is EA${batch}`)
+// }
+
+// DisplayData("PrepBytes", FindSum(10, 9));
+
+// Q9
+// Abc();
+// const Abc = function(){
+//     let value = 20;
+//     console.log(value);
+// }
+
+// // Q10
+// var a = 10;
+// (function (){
+//     console.log(a);
+//     var a = 20;
+// })();
+
+// Q11.
+// const greet =  function(name){
+//     return function(m){
+//         console.log(`Hi!! ${name}, ${m}`);
+//     }
+// }
+  
+// const greet_message = greet('EA19');
+// greet_message("Welcome To PrepBytes")
